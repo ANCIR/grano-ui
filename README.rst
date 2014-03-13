eb296fd94b97e1c53cb4348884477aa31cfb154b
Friedrich Lindenberg <friedrich@pudo.org>
2014-03-13 12:50:11 -0300

    basic python package

---
+++ README.rst
@@ -0,0 +1,11 @@
+Grano Administration Interface
+==============================
+
+``grano`` is a toolkit for building journalistic social network analysis applications on the web. This package contains an administrative user interface based on `AngularJS <http://angularjs.org/>`_. The interface is intended for managing entities 
+and analysis for advanced users, not as a general-purpose interface. 
+
+Installation
+------------
+
+``grano-ui`` requires that you have installed and configured ``grano``. Please refer to grano's documentation for further instructions. Afterwards, install the ``grano-ui`` package (from PyPI or source) into the same virtual environment. 
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='grano-ui',
+    version='0.3.1',
+    description="An entity and social network tracking software for news applications (Admin UI)",
+    long_description=open('README.rst').read(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+    keywords='sql graph sna networks journalism ddj entities',
+    author='Friedrich Lindenberg',
+    author_email='friedrich@pudo.org',
+    url='http://docs.grano.cc',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'grano>=0.3.1'
+    ],
+    tests_require=[]
+)
