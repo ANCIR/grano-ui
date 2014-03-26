4f68da1242d6b55aa51c62928f3914f6d999622b
Friedrich Lindenberg <friedrich@pudo.org>
2014-03-26 21:27:47 +0100

    Finish the schema editor - needs deletion.

--- static/js/controllers/schemata.js
+++ static/js/controllers/schemata.js
@@ -13,7 +13,9 @@ function SchemataIndexCtrl($scope, $routeParams, $location, $http, $modal, $time
     $http.get('/api/1/projects/' + $routeParams.slug + '/schemata', {params: {limit: 1000}}).then(function(res) {
         angular.forEach(res.data.results, function(e) {
             if (e.obj == 'entity') {
-                $scope.entity_schemata.push(e);
+                if (e.name != 'base') {
+                    $scope.entity_schemata.push(e);    
+                }
             } else {
                 $scope.relation_schemata.push(e);
             }
@@ -29,20 +31,50 @@ function SchemataViewCtrl($scope, $routeParams, $location, $http, $route, $modal
     $scope.navSection = 'schemata';
     $scope.project = {};
     $scope.schema = {};
+
+    $scope.newAttributeBase = {fresh: true, datatype: 'string'};
+    $scope.newAttribute = angular.copy($scope.newAttributeBase);
     
     $http.get('/api/1/projects/' + $routeParams.slug).then(function(res) {
         $scope.project = res.data;
     });
 
-    $http.get('/api/1/projects/' + $routeParams.slug + '/schemata/' + $routeParams.name).then(function(res) {
-        $scope.schema = res.data;
-        core.setTitle($scope.schema.label);
-    });
+    if ($routeParams.name) {
+        $http.get('/api/1/projects/' + $routeParams.slug + '/schemata/' + $routeParams.name).then(function(res) {
+            $scope.schema = res.data;
+            core.setTitle($scope.schema.label);
+        });    
+    } else {
+        $scope.schema = {
+            obj: $routeParams.obj,
+            attributes: [],
+            fresh: true,
+            api_url: '/api/1/projects/' + $routeParams.slug + '/schemata'
+        };
+        core.setTitle("New Schema");
+    }
+    
+
+    $scope.addAttribute = function() {
+        $scope.schema.attributes.push($scope.newAttribute);
+        $scope.newAttribute = angular.copy($scope.newAttributeBase);
+    };
+
+    $scope.removeAttribute = function(attribute) {
+        var index = $scope.schema.attributes.indexOf(attribute);
+        if (index > -1) {
+            $scope.schema.attributes.splice(index, 1);
+        }
+    };
 
     $scope.update = function(form) {
         var res = $http.post($scope.schema.api_url, $scope.schema);
         res.success(function(res) {
-            $route.reload();
+            if ($scope.schema.fresh) {
+                $location.path('/p/' + $routeParams.slug + '/schemata/' + res.name);
+            } else {
+                $route.reload();
+            }
         });
         res.error(grano.handleFormError(form));
     };
