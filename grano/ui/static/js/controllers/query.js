e7fcfcf6cfe9b77269ce919b36c68333812f79fb
Friedrich Lindenberg <friedrich@pudo.org>
2014-09-08 21:41:37 +0200

    Factor out graph and table query result displays.

--- grano/ui/static/js/controllers/query.js
+++ grano/ui/static/js/controllers/query.js
@@ -1,41 +1,4 @@
 
-grano.controller('ResultTableCtrl', function ($scope, queryState) {
-  $scope.rows = [];
-  $scope.fields = [];
-
-  $scope.removeColumn = function(field) {
-    var obj = queryState.by_id(field.id);
-    angular.forEach(obj.fields.properties, function(p, i) {
-      if (p.name == field.name)
-        obj.fields.properties.splice(i, 1);
-    });
-    queryState.sync();
-  };
-
-  $scope.$on('queryUpdate', function(event, data) {
-    $scope.fields = queryState.fields();
-    var rows = [];
-    angular.forEach(data, function(row) {
-      var cells = [];
-      angular.forEach($scope.fields, function(field) {
-        var obj = field.get(row),
-            prop = obj.properties[field.name] || {};
-        cells.push({
-          'name': field.name,
-          'type': field.type,
-          'id': obj.id,
-          'schema': field.schame,
-          'value': prop['value']
-        });
-      });
-      rows.push(cells);
-    });
-    $scope.rows = rows;
-  });
-
-});
-
-
 grano.controller('QueryObjectCtrl', function ($scope, queryState, metadata) {
   $scope.schemata = [];
   $scope.attributes = [];
