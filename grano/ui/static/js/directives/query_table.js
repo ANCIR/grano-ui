9d3c7c90ccbb9acbc3de0d771a6282c9b4ca408e
Friedrich Lindenberg <friedrich@pudo.org>
2014-09-08 21:52:07 +0200

    Re-factor the query table to be a directive.

--- grano/ui/static/js/directives/query_table.js
+++ grano/ui/static/js/directives/query_table.js
@@ -1,36 +1,62 @@
+grano.directive('gnQueryTable', ['core', '$http', '$sce', 'queryState', 'metadata',
+    function (core, $http, $sce, queryState, metadata) {
+    return {
+      restrict: 'E',
+      scope: {
+      },
+      templateUrl: 'directives/query_table.html',
+      link: function (scope, element, attrs, model) {
 
-grano.controller('ResultTableCtrl', function ($scope, queryState) {
-  $scope.rows = [];
-  $scope.fields = [];
-
-  $scope.removeColumn = function(field) {
-    var obj = queryState.by_id(field.id);
-    angular.forEach(obj.fields.properties, function(p, i) {
-      if (p.name == field.name)
-        obj.fields.properties.splice(i, 1);
-    });
-    queryState.sync();
-  };
-
-  $scope.$on('queryUpdate', function(event, data) {
-    $scope.fields = queryState.fields();
-    var rows = [];
-    angular.forEach(data, function(row) {
-      var cells = [];
-      angular.forEach($scope.fields, function(field) {
-        var obj = field.get(row),
-            prop = obj.properties[field.name] || {};
-        cells.push({
-          'name': field.name,
-          'type': field.type,
-          'id': obj.id,
-          'schema': field.schame,
-          'value': prop['value']
+        var schemata = [];
+        metadata.getSchemata(function(ss) {
+          schemata = ss;
         });
-      });
-      rows.push(cells);
-    });
-    $scope.rows = rows;
-  });
 
-});
+        scope.rows = [];
+        scope.fields = [];
+
+        scope.getFieldAttribute = function(field, type) {
+          var attribute = {'label': field.name};
+          field.type = field.type || type;
+          angular.forEach(schemata, function(s) {
+            if (s.obj == field.type && s.name == field.schema) {
+              angular.forEach(s.attributes, function(a) {
+                if (a.name == field.name) attribute = a;
+              });
+            }
+          });
+          return attribute;
+        };
+
+        scope.removeColumn = function(field) {
+          var obj = queryState.by_id(field.id);
+          angular.forEach(obj.fields.properties, function(p, i) {
+            if (p.name == field.name)
+              obj.fields.properties.splice(i, 1);
+          });
+          queryState.sync();
+        };
+
+        scope.$on('queryUpdate', function(event, data) {
+          scope.fields = queryState.fields();
+          var rows = [];
+          angular.forEach(data, function(row) {
+            var cells = [];
+            angular.forEach(scope.fields, function(field) {
+              var obj = field.get(row),
+                  prop = obj.properties[field.name] || {};
+              cells.push({
+                'name': field.name,
+                'type': field.type,
+                'id': obj.id,
+                'schema': field.schame,
+                'value': prop['value']
+              });
+            });
+            rows.push(cells);
+          });
+          scope.rows = rows;
+        });
+      }
+    };
+}]);
