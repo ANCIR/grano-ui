3b6b0e913a2187e9219bcfe9a0f746e2eaa45cea
Friedrich Lindenberg <friedrich@pudo.org>
2014-09-09 15:28:24 +0200

    Refactor ObjectQueryCtrl into gn-query-panel-layer.

--- grano/ui/static/js/directives/query_panel.js
+++ grano/ui/static/js/directives/query_panel.js
@@ -1,102 +1,122 @@
 grano.directive('gnQueryPanel', ['queryUtils', function(queryUtils) {
-    return {
-      restrict: 'EA',
-      scope: {
-        'project': '='
-      },
-      link: function(scope, element, attrs) {
-        scope.layers = [];
+  return {
+    restrict: 'EA',
+    scope: {
+    'project': '='
+    },
+    templateUrl: 'directives/query_panel.html',
+    link: function(scope, element, attrs) {
+    scope.layers = [];
 
-        var pack = function(layers) {
-            var layer = layers[0],
-                tail = layers.slice(1),
-                query = {};
+    var pack = function(layers) {
+      var layer = layers[0],
+        tail = layers.slice(1),
+        query = {};
 
-            angular.forEach(['fields', 'filters'], function(s) {
-                angular.forEach(layer[s], function(v, k) {
-                    if (k == 'properties') {
-                        v = angular.extend(query.properties || {}, v);
-                    }
-                    query[k] = v;
-                });
-            });
+      angular.forEach(['fields', 'filters'], function(s) {
+        angular.forEach(layer[s], function(v, k) {
+          if (k == 'properties') {
+            v = angular.extend(query.properties || {}, v);
+          }
+          query[k] = v;
+        });
+      });
 
-            var propcount = 0;
-            angular.forEach(query.properties, function() { propcount++; });
-            if (propcount == 0) {
-                delete query.properties;
-            }
+      var propcount = 0;
+      angular.forEach(query.properties, function() { propcount++; });
+      if (propcount == 0) {
+        delete query.properties;
+      }
 
-            if (tail.length) {
-                if (layer.obj == 'relation') {
-                    query['other'] = pack(tail);
-                } else {
-                    query['relations'] = pack(tail);
-                }
-            }
-            return layer.as_list ? new Array(query) : query;
-        };
+      if (tail.length) {
+        if (layer.obj == 'relation') {
+          query['other'] = pack(tail);
+        } else {
+          query['relations'] = pack(tail);
+        }
+      }
+      return layer.as_list ? new Array(query) : query;
+    };
 
-        var unpack = function(query, depth) {
-            var layer = {
-                as_list: false,
-                fields: {properties: {}},
-                filters: {properties: {}},
-                root: depth == 0,
-                obj: (depth % 2) == 0 ? 'entity' : 'relation'
-            };
-            if (angular.isArray(query)) {
-                layer.as_list = true;
-                query = query.length ? query[0] : null;
-            }
+    var unpack = function(query, depth) {
+      var layer = {
+        as_list: false,
+        fields: {properties: {}},
+        filters: {properties: {}},
+        root: depth == 0,
+        obj: (depth % 2) == 0 ? 'entity' : 'relation'
+      };
+      if (angular.isArray(query)) {
+        layer.as_list = true;
+        query = query.length ? query[0] : null;
+      }
 
-            var get = function(name, obj, key, prefix) {
-                if (!angular.isUndefined(obj)) {
-                    if (angular.isArray(obj)) {
-                        if (obj.length) {
-                            obj = obj[0];
-                        } else {
-                            obj = null;
-                        }
-                    } 
-                    if (angular.isObject(obj) && key) {
-                        obj = obj[key];
-                    }
-                    
-
-                    if (obj == null) {
-                        var target = layer.fields;
-                        if (prefix) target = target[prefix];
-                        target[name] = obj;
-                    } else {
-                        var target = layer.filters;
-                        if (prefix) target = target[prefix];
-                        target[name] = obj;
-                    }
-                }
-            }
-
-            get('id', query.id)
-            if (layer.obj == 'entity') {
-                get('schemata', query.schemata, 'name');
+      var get = function(name, obj, key, prefix) {
+        if (!angular.isUndefined(obj)) {
+          if (angular.isArray(obj)) {
+            if (obj.length) {
+              obj = obj[0];
             } else {
-                get('schema', query.schema, 'name');
+              obj = null;
             }
-            angular.forEach(query.properties || {}, function(v, k) {
-                get(k, v, 'value', 'properties');
-            });
+          } 
+          if (angular.isObject(obj) && key) {
+            obj = obj[key];
+          }
+          
 
-            var next = queryUtils.nextLevel(query);
-            return next == null ? [layer] : [layer].concat(unpack(query[next], depth+1));
-        };
+          if (obj == null) {
+            var target = layer.fields;
+            if (prefix) target = target[prefix];
+            target[name] = obj;
+          } else {
+            var target = layer.filters;
+            if (prefix) target = target[prefix];
+            target[name] = obj;
+          }
+        }
+      }
 
-        var q = [{
-            'id': null, 'properties': {'name': {'value': null}}, 'relations': [{'schema': 'foo', 'other': {'id': null}}]
-        }];
-        var unpacked = unpack(q, 0);
-        console.log(unpacked);
-        var packed = pack(unpacked);
-        console.log(packed);
+      get('id', query.id)
+      if (layer.obj == 'entity') {
+        get('schemata', query.schemata, 'name');
+      } else {
+        get('schema', query.schema, 'name');
+      }
+      angular.forEach(query.properties || {}, function(v, k) {
+        get(k, v, 'value', 'properties');
+      });
 
-    }};
+      var next = queryUtils.nextLevel(query);
+      return next == null ? [layer] : [layer].concat(unpack(query[next], depth+1));
+    };
+
+    scope.$on('query', function(e, name, query) {
+      if (name == 'root') {
+        scope.layers = unpack(query, 0);
+      }
+    });
+
+    /*
+    var q = [{
+      'id': null, 'properties': {'name': {'value': null}}, 'relations': [{'schema': 'foo', 'other': {'id': null}}]
+    }];
+    var unpacked = unpack(q, 0);
+    console.log(unpacked);
+    var packed = pack(unpacked);
+    console.log(packed);
+    */
+
+    scope.addLayers = function() {
+      var lastObject = scope.layers[scope.layers.length-1];
+      if (lastObject.obj == 'entity') {
+        scope.layers.push({'obj': 'relation',
+                'fields': {'properties': {}},
+                'filters': {'properties': {}}});
+      }
+      scope.layers.push({'obj': 'entity',
+              'fields': {'properties': {'name': 'name'}},
+              'filters': {'properties': {}}});
+    };
+  }};
 }]);
--- grano/ui/static/js/directives/query_panel_layer.js
+++ grano/ui/static/js/directives/query_panel_layer.js
@@ -1,12 +1,108 @@
-grano.directive('gnQueryPanelLayer', [function() {
-    return {
-      restrict: 'EA',
-      scope: {
-        'project': '=',
-        'layer': '='
-      },
-      link: function(scope, element, attrs) {
-        
-        }
-    };
+grano.directive('gnQueryPanelLayer', ['metadata', function(metadata) {
+  return {
+    restrict: 'EA',
+    scope: {
+    'project': '=',
+    'layer': '='
+    },
+    templateUrl: 'directives/query_panel_layer.html',
+    link: function(scope, element, attrs) {
+      scope.schemata = [];
+      scope.attributes = [];
+      scope.visibleSchemata = [];
+
+      scope.setSchema = function(e) {
+        scope.layer.filters.schema = e.name;
+      };
+
+      scope.removeLayer = function() {
+        alert("TODO");
+      };
+
+      scope.getSchemaLabel = function() {
+        var label = scope.anyLabel();
+        angular.forEach(scope.schemata, function(s) {
+          if (scope.object.filters['schema'] == s.name) {
+            label = s.meta.plural_upper || s.label;
+          }
+        });
+        return label;
+      };
+
+      scope.anyLabel = function() {
+        if (scope.layer.obj == 'relation') return 'Any relation type';
+        return 'Any entities';
+      };
+
+      scope.actionLabel = function() {
+        if (scope.layer.root) return 'Find';
+        if (scope.layer.obj == 'relation') return 'connected via';
+        return 'to';
+      };
+
+      scope.availableFields = function () {
+        var attributes = [];
+        angular.forEach(scope.attributes, function(a) {
+          if (a.hidden) return;
+          var taken = false;
+          angular.forEach(scope.object.fields.properties, function(p) {
+            if (p.name == a.name && p.schema == a.schema.name) taken = true;
+          });
+          if (!taken) attributes.push(a);
+        });
+        return attributes;
+      };
+
+      scope.availableFilters = function () {
+        var attributes = [];
+        angular.forEach(scope.attributes, function(a) {
+          if (a.hidden) return;
+          var taken = false;
+          angular.forEach(scope.layer.filters.properties, function(p) {
+            if (p.name == a.name && p.schema == a.schema.name) taken = true;
+          });
+          if (!taken) attributes.push(a);
+        });
+        return attributes;
+      };
+
+      scope.addField = function(attr) {
+        scope.layer.fields.properties[attr.name] = null;
+      };
+
+      scope.addFilter = function(attr) {
+        scope.layer.filters.properties[attr.name] = '';
+      };
+
+      scope.$watch('layer', function(layer) {
+        if (!layer) return;
+
+        metadata.getSchemata().then(function(s) {
+
+          var visible = [{'name': null, 'label': scope.anyLabel()}],
+              attributes = [];
+
+          angular.forEach(s, function(sc) {
+            if (sc.obj != layer.obj) {
+              return;
+            }
+            
+            if (!sc.hidden) visible.push(sc);
+            if ((layer.filters && sc.name == layer.filters.schema) || sc.default) {
+              angular.forEach(sc.attributes, function(a) {
+                var at = angular.copy(a);
+                at['schema'] = sc;
+                attributes.push(at);
+              });
+            }
+          });
+
+          scope.visibleSchemata = visible;
+          scope.schemata = s;
+          scope.attributes = attributes;
+        });
+
+      });
+    }
+  };
 }]);
