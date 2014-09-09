5bead039843399dc507bc0271bda1909d3a128d4
Friedrich Lindenberg <friedrich@pudo.org>
2014-09-09 14:44:00 +0200

    Split up query panel template.

---
+++ grano/ui/static/js/directives/query_panel_layer.js
@@ -0,0 +1,12 @@
+grano.directive('gnQueryPanelLayer', [function() {
+    return {
+      restrict: 'EA',
+      scope: {
+        'project': '=',
+        'layer': '='
+      },
+      link: function(scope, element, attrs) {
+        
+        }
+    };
+}]);
