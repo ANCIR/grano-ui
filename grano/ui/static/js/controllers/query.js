f380a9704d00c726741e33e21abee4cac02f59a5
Friedrich Lindenberg <friedrich@pudo.org>
2014-09-09 15:44:47 +0200

    Clean out old getAttribute function, needs replacement

--- grano/ui/static/js/controllers/query.js
+++ grano/ui/static/js/controllers/query.js
@@ -8,7 +8,6 @@ function QueryCtrl($scope, $routeParams, $http, $q, metadata, queryState) {
   });
 
   $scope.loading = false;
-  $scope.objects = queryState.objects;
 
   $scope.$on('querySend', function() {
     $scope.loading = true;
@@ -18,19 +17,6 @@ function QueryCtrl($scope, $routeParams, $http, $q, metadata, queryState) {
     $scope.loading = false;
   });
 
-  $scope.getFieldAttribute = function(field, type) {
-    var attribute = {'label': field.name};
-    field.type = field.type || type;
-    angular.forEach(allSchemata, function(s) {
-      if (s.obj == field.type && s.name == field.schema) {
-        angular.forEach(s.attributes, function(a) {
-          if (a.name == field.name) attribute = a;
-        });
-      }
-    });
-    return attribute;
-  };
-  
   queryState.init();
   queryState.sync();
 };
